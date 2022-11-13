be9c03f60eb60643b621975ee308f93e5582bff0
beanaccle <25994266+beanaccle@users.noreply.github.com>
2022-11-13 12:15:58 +0900

    add Users::PurchaseProduct

--- app/models/users/purchase_product.rb
+++ app/models/users/purchase_product.rb
@@ -6,7 +6,11 @@ module Users
 
     def call(product_id)
       product = Product.find(product_id)
-      PointPayment.new(@user).call(product.user, product.price)
+
+      ActiveRecord::Base.transaction do
+        PointPayment.new(@user).call(product.user, product.price)
+        CreatePaymentHistory.new(@user).call(product)
+      end
 
       product
     end
--- spec/models/users/purchase_product_spec.rb
+++ spec/models/users/purchase_product_spec.rb
@@ -2,34 +2,49 @@ require 'rails_helper'
 
 RSpec.describe Users::PurchaseProduct do
   describe "#call" do
-    let(:user) { create(:user) }
-    let(:product) { create(:product, user: user) }
-
-    before "mock Users::PointPayment" do
-      @point_payment_mock = instance_double(Users::PointPayment)
-      allow(Users::PointPayment).to receive(:new).and_return(@point_payment_mock)
-      allow(@point_payment_mock).to receive(:call)
-    end
+    let(:buyer) { create(:user, email: "buyer@example.com", points: 100_000) }
+    let(:seller) { create(:user, email: "seller@example.com", points: 0) }
+    let(:product) { create(:product, user: seller, price: 50_000) }
 
     context "when product found" do
-      it "called Users::PointPayment" do
-        described_class.new(user).call(product.id)
+      it "points are transferred" do
+        expect {
+          described_class.new(buyer).call(product.id)
+          buyer.reload
+          seller.reload
+        }.to change(buyer, :points).from(100_000).to(50_000)
+        .and change(seller, :points).from(0).to(50_000)
+      end
 
-        expect(@point_payment_mock).to have_received(:call).once
+      it "payment_history is created" do
+        expect {
+          described_class.new(buyer).call(product.id)
+        }.to change(PaymentHistory, :count).by(1)
       end
 
       it "returns product instance" do
-        expect(described_class.new(user).call(product.id)).to be_a(Product)
+        expect(described_class.new(buyer).call(product.id)).to be_a(Product)
+      end
+
+      context "when failed to save payment_history" do
+        it "points are not transferred" do
+          allow(Users::CreatePaymentHistory).to receive(:new).and_raise(ActiveRecord::RecordInvalid)
+
+          expect {
+            described_class.new(buyer).call(product.id)
+          }.to raise_error(ActiveRecord::RecordInvalid)
+
+          expect(buyer.reload.points).to eq(100_000)
+          expect(seller.reload.points).to eq(0)
+        end
       end
     end
 
     context "when product not found" do
       it "not calling Users::PointPayment" do
         expect {
-          described_class.new(user).call(0)
+          described_class.new(buyer).call(0)
         }.to raise_error(ActiveRecord::RecordNotFound)
-
-        expect(@point_payment_mock).not_to have_received(:call)
       end
     end
   end
